cf2b63306947f2fee52dff4f8d4c8b1ea82d327a
f3pe <fel234bf4@gmail.com>
2025-02-07 01:00:42 -0300

    Primeira versão totalmente funcional

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,6 @@
-let concentrationTime = 150000, breakTime = 300;
+let focusTime = 150000, breakTime = 30000;
 
-let interval = 0, timer = concentrationTime; 
+let interval = 0, timer = focusTime; 
 
 function formatTime(time){
     const minutes = Math.floor((time % 360000) / 6000);
@@ -9,7 +9,7 @@ function formatTime(time){
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
-function setTimer(timer){
+function setTimer(){
     document.getElementById('timer').innerHTML = formatTime(timer);
 }
 
@@ -20,7 +20,7 @@ function toggleTimer() {
     if (mode == 'play' || mode == 'continue'){
         interval = setInterval(() => {
             timer -= 1;
-            setTimer(timer);
+            setTimer();
             if (timer <= 0){clearInterval(interval);}
         }, 10);
         playPauseButton.setAttribute('action', 'pause');
@@ -31,4 +31,27 @@ function toggleTimer() {
     }
 }
 
+function changeMode(){
+    clearInterval(interval);
+    let breakFocusButton = document.getElementById('break');
+    let clockMode = breakFocusButton.getAttribute('action');
+
+    if (clockMode == 'break'){
+        timer = breakTime
+        setTimer();
+        breakFocusButton.setAttribute('action', 'focus');
+        breakFocusButton.innerHTML = '<i class="fa-solid fa-bullseye"></i>';
+    }else{
+        timer = focusTime
+        setTimer();
+        breakFocusButton.setAttribute('action', 'break');
+        breakFocusButton.innerHTML = '<i class="fa-solid fa-mug-hot"></i>';
+    }
+    
+    let playPauseButton = document.getElementById('play');
+    playPauseButton.setAttribute('action', 'continue');
+    playPauseButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+}
+
 document.getElementById('play').addEventListener("click", toggleTimer);
+document.getElementById('break').addEventListener("click", changeMode);
